5c551644629985afd69dfda9215160bacaea7949
Nurmuhammad0034 <orifjonovnurmuhammad0@gmail.com>
2023-06-24 18:05:16 +0500

    comment ochildi

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
-# while True:
-#     try:
-#         num1 = int(input("Enter a first number: "))
-#         num2 = int(input("Enter a second number: "))
-#         amal = input("Enter an operation (+-*/) or q for quit: ")
+while True:
+    try:
+        num1 = int(input("Enter a first number: "))
+        num2 = int(input("Enter a second number: "))
+        amal = input("Enter an operation (+-*/) or q for quit: ")
 
-#         if amal == '+':
-#             print(f"{num1}+{num2}={num1+num2}")
-#         elif amal == '-':
-#             print(f"{num1}-{num2}={num1-num2}")
-#         elif amal == '*':
-#             print(f"{num1}*{num2}={num1*num2}")
-#         elif amal == '/':
-#             print(f"{num1}/{num2}={num1/num2}")
-#         elif amal == 'q':
-#             break
-#         else:
-#             print("Operator noto'g'ri kiritildi.")
+        if amal == '+':
+            print(f"{num1}+{num2}={num1+num2}")
+        elif amal == '-':
+            print(f"{num1}-{num2}={num1-num2}")
+        elif amal == '*':
+            print(f"{num1}*{num2}={num1*num2}")
+        elif amal == '/':
+            print(f"{num1}/{num2}={num1/num2}")
+        elif amal == 'q':
+            break
+        else:
+            print("Operator noto'g'ri kiritildi.")
 
-#     except ZeroDivisionError:
-#         print("Nolga bo'lish mumkin emas!")
-#     except ValueError:
-#         print("Son kiritilmadi!")
-#     except:
-#         print("Dasturda xatolik mavjud")
-#     else:
-#         print("Keyingi sonlarni kiriting.")
-#     finally:
-#         print("Good bye. Dasturdan foydalanganingiz uchun rahmat :D")
+    except ZeroDivisionError:
+        print("Nolga bo'lish mumkin emas!")
+    except ValueError:
+        print("Son kiritilmadi!")
+    except:
+        print("Dasturda xatolik mavjud")
+    else:
+        print("Keyingi sonlarni kiriting.")
+    finally:
+        print("Good bye. Dasturdan foydalanganingiz uchun rahmat :D")
